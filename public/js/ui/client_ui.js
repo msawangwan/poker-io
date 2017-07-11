1a7194429bf3853df23c75ccc9e537447b3965df
misha sawangwan <misha.sawangwan@gmail.com>
2017-07-11 13:05:02 -0700

    actually draw a proper poker table wip

--- public/js/ui/client_ui.js
+++ public/js/ui/client_ui.js
@@ -3,9 +3,9 @@ const div = content => $('<div></div>').text(content);
 
 const table = {
     dimensions: {
-        radius: 150,
+        radius: 450,
         scale: 0.25,
-        spacing: 40 // 360 / 40 === 9 seats
+        spacing: 360 / 12 // 360 / 40 === 9 seats
     },
     seating: {
         positions: new Map()
@@ -21,42 +21,40 @@ function ImageCache() {
     this.store = new Map();
 }
 
-ImageCache.prototype.load = function(assetpath) {
+ImageCache.prototype.load = function (assetpath) {
     if (this.store.has(assetpath)) {
         return this.store.get(assetpath);
     }
-    
+
     const img = new Image();
-    
-    img.onload = () => {}
+
+    img.onload = () => { }
     img.src = assetpath;
-    
+
     this.store.set(assetpath, img);
 };
 
 // sheet = parent spritesheet
 // framedata = frame width, frame height, frame index start, frame index end
-function Sprite(sheet, framedata) {
+function Sprite(sheet, framedata, onload) {
     this.src = sheet;
-    
-    const {w, h, s, e } = framedate;
-    
+
+    const { w, h, s, e } = framedate;
+
     this.frame = {
         width: w, height: h, start: s, end: e
     }
 }
 
-function Sprite.prototype.load = function() {
-    
-}
+Sprite.prototype.load = function () {
+
+};
 
 $(document).ready(() => {
     const socket = io.connect(window.location.origin);
+
     const canvas = document.getElementById('table-canvas');
     const ctx = canvas.getContext('2d');
-
-    const img_cardback = new Image();
-    const img_cardsheet = new Image();
 
     const canvasAxis = {
         width: 0,
@@ -84,94 +82,155 @@ $(document).ready(() => {
         return true;
     };
 
-    const drawTablePlayerPositions = () => {
+    const drawTableSeating = seatedAt => {
         const tableradius = table.dimensions.radius;
 
         const originx = currentCanvasCenter.x();
         const originy = currentCanvasCenter.y();
 
-        img_cardback.onload = () => {
-            const scale = table.dimensions.scale;
+        const seatw = 64;
+        const seath = 64;
 
-            const imgw = img_cardback.width * scale;
-            const imgh = img_cardback.height * scale;
+        const scaledseatw = seatw * table.dimensions.scale;
+        const scaledseath = seath * table.dimensions.scale;
 
-            const offsetx = imgw / 2;
-            const offsety = imgh / 2;
+        const offsetx = scaledseatw / 2;
+        const offsety = scaledseath / 2;
 
-            const step = table.dimensions.spacing;
+        const scalewidth = 0.85;
+        const scaleheight = 0.35;
 
-            for (let theta = 0; theta < 360; theta += step) {
-                const rads = toRadians(theta);
+        const step = table.dimensions.spacing;
 
-                const x = (originx + tableradius * Math.cos(rads)) - offsetx;
-                const y = (originy + tableradius * Math.sin(rads)) - offsety;
+        const start = { x: 0, y: 0 };
+        const tableoffset = 50; // aka radius
 
-                ctx.drawImage(img_cardback, x, y, imgw, imgh);
-            }
-        };
+        ctx.fillStyle = 'green';
+        ctx.beginPath();
 
-        img_cardback.src = cardbackpair;
+        ctx.arc(originx, originy, tableoffset, Math.PI * 0.5, Math.PI * 0.50 + Math.PI);
+        ctx.moveTo(originx, originy + tableoffset);
+        ctx.lineTo(originx + 100, originy + tableoffset);
+
+        // for (let theta = 0; theta < 360; theta += step) {
+        //     const rads = toRadians(theta);
+
+        //     const x = (originx + tableradius * scalewidth * Math.cos(rads)) - offsetx;
+        //     const y = (originy + tableradius * scaleheight * Math.sin(rads)) - offsety;
+
+        //     if (theta === 0) {
+        //         start.x = x;
+        //         start.y = y;
+        //     }
+
+        //     ctx.lineTo(x, y);
+        // }
+
+        // ctx.lineTo(start.x, start.y);
+        ctx.stroke();
+        // ctx.fill();
     };
 
-    const createCard = (spritesheet, w, h) => {
-        return {
-            img: spritesheet,
-            width: w,
-            height: h,
-        }
+    const player = {
+        seat: -1
     };
 
-    const getCardAtIndex = (s, v) => {
-        console.log(s + ' ' + v);
-        ctx.drawImage(
-            img_cardsheet,
-            v * cardpixelwidth, // frame index * frame width
-            s * cardpixelheight, // frame row?
-            cardpixelwidth, // frame width
-            cardpixelheight, // frame height
-            s * v, // dest x
-            s, // dest y
-            cardpixelwidth, // frame width on draw (same as input usually)
-            cardpixelheight // frame height on draw (same as input usually)
-        );
-    };
+    socket.on('ack-client-connect-success', e => {
+        player.seat = e.assignedseat;
 
-    let suite = 0;
-    let v = 0;
+        socket.emit('client-request-seating-update', { playerseatindex: player.seat });
+    });
 
-    img_cardsheet.onload = () => {
-        setInterval(() => {
-            if (suite >= 4) {
-                suite = 0;
-            }
+    socket.on('ack-client-connect-fail', e => {
+        alert(e.reason);
+    });
 
-            if (v >= 13) {
-                v = 0;
-            }
-
-            getCardAtIndex(suite, v);
-            suite += 1;
-            v += 1;
-        }, 750);
-    };
-
-    img_cardsheet.src = cardspritesheet;
-
-    socket.on('update-ui-display-table', state => {
+    socket.on('server-response-seating-update', e => {
+        console.log(e.seatingstate);
         updateCanvasDimensions();
-        drawTablePlayerPositions();
-        for (const s of state.tableState) {
-            if (s === 'empty seat') {
-                // seat is empty
-            } else {
-
-            }
-        }
+        drawTableSeating(player.seat);
     });
 
     $(window).on('resize', () => {
         updateCanvasDimensions();
-        drawTablePlayerPositions();
+        drawTableSeating(player.seat);
     });
+
+    // const img_cardback = new Image();
+    // const drawTableSeating = seatedAt => {
+    //     const tableradius = table.dimensions.radius;
+
+    //     const originx = currentCanvasCenter.x();
+    //     const originy = currentCanvasCenter.y();
+
+    //     const scale = table.dimensions.scale;
+
+    //     const imgw = img_cardback.width * scale;
+    //     const imgh = img_cardback.height * scale;
+
+    //     const offsetx = imgw / 2;
+    //     const offsety = imgh / 2;
+
+    //     const step = table.dimensions.spacing;
+
+    //     for (let theta = 0; theta < 360; theta += step) {
+    //         const rads = toRadians(theta);
+
+    //         const x = (originx + tableradius * 0.25 * Math.cos(rads)) - offsetx;
+    //         const y = (originy + tableradius * Math.sin(rads)) - offsety;
+
+    //         ctx.font = '24px serif';
+    //         ctx.fillText(theta, x, y);
+    //     }
+    // };
+
+    // socket.on('update-ui-display-table', state => {
+    //     updateCanvasDimensions();
+    //     drawTableSeating();
+    //     for (const s of state.tableState) {
+    //         if (s === 'empty seat') {
+    //             // seat is empty
+    //         } else {
+
+    //         }
+    //     }
+    // });
+
+    // const img_cardsheet = new Image();
+
+    // const getCardAtIndex = (s, v) => {
+    //     console.log(s + ' ' + v);
+    //     ctx.drawImage(
+    //         img_cardsheet,
+    //         v * cardpixelwidth, // frame index * frame width
+    //         s * cardpixelheight, // frame row?
+    //         cardpixelwidth, // frame width
+    //         cardpixelheight, // frame height
+    //         s * v, // dest x
+    //         s, // dest y
+    //         cardpixelwidth, // frame width on draw (same as input usually)
+    //         cardpixelheight // frame height on draw (same as input usually)
+    //     );
+    // };
+
+    // let suite = 0;
+    // let v = 0;
+
+    // img_cardsheet.onload = () => {
+    //     setInterval(() => {
+    //         if (suite >= 4) {
+    //             suite = 0;
+    //         }
+
+    //         if (v >= 13) {
+    //             v = 0;
+    //         }
+
+    //         getCardAtIndex(suite, v);
+    //         suite += 1;
+    //         v += 1;
+    //     }, 750);
+    // };
+
+    // img_cardsheet.src = cardspritesheet;
 });
