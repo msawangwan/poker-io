103e33f1f2129d027cb835c5cb077e9cfbbbfad2
misha 'madmeesh' sawangwan (@roadwarrior) <misha@sawangwan.me>
2017-07-19 19:10:53 -0700

    added a label manager module

---
+++ public/js/model/labelmanager.js
@@ -0,0 +1,33 @@
+class LabelManager {
+    constructor() {
+        this.cache = new Map();
+    };
+    
+    add(name, label, calcx, calcy) {
+        const group = this.cache.get(name);
+        
+        if (!group) {
+            return false;
+        }
+        
+        group.push({
+            label: label,
+            calcx: calcx,
+            calcy: calcy
+        });
+    
+        this.cache.set(name) = group;
+        
+        return true;
+    };
+    
+    recalc(name) {
+        const group = this.cache.get(name);
+        
+        if (group) {
+            for (const l of group) {
+                
+            }
+        }
+    }
+}
--- public/js/model/labelrenderer.js
+++ public/js/model/labelrenderer.js
@@ -1,55 +1,73 @@
-function LabelRenderer() {
-    this.labels = new Map();
-
-    this.currentid = -1;
-    this.labelid = () => this.currentid += 1;
-
-    this.labelNode = document.getElementById('temp-container');
+class LabelRenderer {
+    constructor() {
+        this.labels = new Map();
+    
+        // this.currentid = -1;
+        // this.labelid = () => this.currentid += 1;
+    
+        // this.labelNode = document.getElementById('temp-container');
+    };
+    
+    render (ctx) {
+        for (const [id, next] of this.labels) {
+            if (next.drawOnNextTick) {
+                next.render(ctx);
+            }
+        }
+    };
+    
+    add (text, fontstyle, fontsize, color) {
+        const l = new Label(text, fontstyle, fontsize, color, id);
+        this.labels.set(l.id, l);
+        return l.id;
+    };
+    
+    remove (ctx, id) {
+        const label = this.labels.get(id);
+    
+        if (label) {
+            globalctx.clearRect(label.transform.global.x, label.transform.global.y, label.canvas.width, label.canvas.height);
+            const node = document.getElementById(label.id);
+            if (node) {
+                node.parentNode.removeChild(node);
+                this.labels.delete(id);
+    
+                return true;
+            }
+        }
+    
+        return false;
+    };
+    
+    update (id, ctx, x, y, text) {
+        let old = null;
+    
+        if (this.labels.has(id)) {
+            old = this.labels.get(id);
+            this.remove(ctx, id);
+        }
+    
+        const newid = this.add(text, old.font.style, old.font.size, old.font.color);
+        this.labels.get(newid).updateTransform(ctx, x, y);
+    
+        return newid;
+    };
 }
 
-LabelRenderer.prototype.render = function (ctx) {
-    for (const [id, next] of this.labels) {
-        if (next.drawOnNextTick) {
-            next.render(ctx);
-        }
-    }
-};
+// function LabelRenderer() {
+// }
 
-LabelRenderer.prototype.add = function (text, fontstyle, fontsize, color) {
-    const id = this.labelid();
-    this.labels.set(id, new Label(text, fontstyle, fontsize, color, id));
-    return id;
-};
+// LabelRenderer.prototype.render = function (ctx) {
+// };
 
-LabelRenderer.prototype.remove = function (globalctx, id) {
-    const label = this.labels.get(id);
+// LabelRenderer.prototype.add = function (text, fontstyle, fontsize, color) {
+    // const id = this.labelid();
+    // this.labels.set(id, new Label(text, fontstyle, fontsize, color, id));
+    // return id;
+// };
 
-    if (label) {
-        globalctx.clearRect(label.transform.global.x, label.transform.global.y, label.canvas.width, label.canvas.height);
-        const node = document.getElementById(label.id);
-        if (node) {
-            node.parentNode.removeChild(node);
-            this.labels.delete(id);
+// LabelRenderer.prototype.remove = function (globalctx, id) {
+// };
 
-            return true;
-        }
-    }
-
-    return false;
-};
-
-LabelRenderer.prototype.update = function (id, ctx, x, y, text) {
-    let old = null;
-
-    if (this.labels.has(id)) {
-        old = this.labels.get(id);
-        this.remove(ctx, id);
-    }
-
-    const newid = this.add(text, old.font.style, old.font.size, old.font.color);
-    console.log(old);
-    console.log(this.labels.get(newid));
-    this.labels.get(newid).updateTransform(ctx, x, y);
-
-    return newid;
-};
+// LabelRenderer.prototype.update = function (id, ctx, x, y, text) {
+// };
