ab2a04a90c2c286391b76c935c5eb37c3de8e151
misha sawangwan <misha.sawangwan@gmail.com>
2017-07-25 11:17:02 -0700

    timestamp the html logger output

--- public/js/util/htmllogger.js
+++ public/js/util/htmllogger.js
@@ -1,9 +1,10 @@
 class HTMLLogger {
     constructor() {
         this.output = document.getElementById('table-log-output');
-    }
+        this.timestamp = new Date();
+    };
 
     log(message) {
-        this.output.insertAdjacentHTML('beforeend', `<small>${message}</small>`)
-    }
+        this.output.insertAdjacentHTML('beforeend', `<small>[${this.timestamp.toLocaleString()}] ${message}</small>`)
+    };
 }
