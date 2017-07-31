14c0186c44844cbecf6e6f2a8bcd7fd2571ef6d0
misha sawangwan <misha@sawangwan.me>
2017-07-30 22:53:15 -0700

    canvas sprite

--- public/js/model/canvassprite.js
+++ public/js/model/canvassprite.js
@@ -12,19 +12,48 @@ class TableSprite extends CanvasSprite {
         super(parentcanvas, label);
     }
 
-    draw(x, y, w, h) {
-        const ctx = this.canvas.getContext('2d');
-        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    draw(x, y, r, w, h, dx, dy) {
+        this.canvas.width = w;
+        this.canvas.height = h;
 
-        this.canvas.width = this.dimensions.w;
-        this.canvas.height = this.dimensions.h;
+        const ctx = this.canvas.getContext('2d');
+
+        const ox = this.parentcanvas.width * 0.65;
+        const oy = this.parentcanvas.height * 0.65
+        /*
+            this.canvasorigin.x - this.dimensions.off, this.canvasorigin.y, this.dimensions.r
+            this.canvasorigin.x + this.dimensions.off, this.canvasorigin.y, this.dimensions.r
+        */
 
         ctx.beginPath();
-        ctx.arc(this.canvasorigin.x - this.dimensions.off, this.canvasorigin.y, this.dimensions.r, Math.PI * 0.5, Math.PI * 0.5 + Math.PI);
-        ctx.arc(this.canvasorigin.x + this.dimensions.off, this.canvasorigin.y, this.dimensions.r, Math.PI * 0.5 + Math.PI, Math.PI * 0.5);
+        ctx.arc(x, y, r, Math.PI * 0.5, Math.PI * 0.5 + Math.PI);
+        ctx.arc(x, y, r, Math.PI * 0.5 + Math.PI, Math.PI * 0.5);
         ctx.fillStyle = 'green';
         ctx.fill();
 
-        this.parentcanvas.getContext('2d').drawImage(this.canvas, this.postion.x, this.postion.y);
+        this.parentcanvas.getContext('2d').drawImage(this.canvas, dx, dy);
+    }
+}
+
+class TableSeatSprite extends CanvasSprite {
+    constructor(parentcanvas, label) {
+        super(parentcanvas, label);
+    }
+
+    draw(x, y, r, w, h, dx, dy) {
+        this.canvas.width = w;
+        this.canvas.height = h;
+
+        const ctx = this.canvas.getContext('2d');
+
+        /*
+            this.canvas.width / 2, this.canvas.height / 2, this.dimensions.r
+        */
+
+        ctx.arc(x, y, r, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+
+        this.parentcanvas.getContext('2d').drawImage(this.canvas, dx, dy);
     }
 }
